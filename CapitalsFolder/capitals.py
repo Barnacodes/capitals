c23d91e689e381665bec41d879c5a1062c728301
Barnacodes <barnaba.pellini@student.h-farm.com>
2019-12-03 16:02:34 +0100

    properly documented the code

--- CapitalsFolder/capitals.py
+++ CapitalsFolder/capitals.py
@@ -1,3 +1,5 @@
+"""Here we have a dictionary in which there is a list. In this list, each line is composed by a state(key) and his capital(value)"""
+
 list_of_capitals = {'Aland Islands':'Mariehamn',
 'Albania':'Tirana',
 'Andorra':'Andorra la Vella',
@@ -57,11 +59,18 @@ list_of_capitals = {'Aland Islands':'Mariehamn',
 'United Kingdom':'London',
 'Vatican City':'Vatican City'}
 
+
+"""This function checks if the input is a state that appears in the list_of_capitals. If the state is within the list, it prints the state’s name and his capital; if not, it prints that the state’s name does not 
+seem to be an European State"""
+
 def check_capital(state_name):
     if state_name in list_of_capitals:
         print("The capital of {} is {}".format(state_name, list_of_capitals[state_name]))
     else:
         print("Sorry, {} does not seem to be an European state".format(state_name))
+
+
+"""This function checks if the input is a capital that appears in the list_of_capitals. If the capital is within the list, it prints the capital’s name and his state; if not, it prints that the capital does not seem to be an European Capital."""
 
 def check_state(capital_name):
     for state, capital in list_of_capitals.items():
