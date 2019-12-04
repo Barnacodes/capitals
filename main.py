c54a93e15eb24170b79824e16773bba476b1fd19
LaAliss <alice.rigotto@student.h-farm.com>
2019-12-04 04:42:59 -0500

    modified main.py with sys.argv module

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python3\
 
+'''In the main.py we are importing the module check from mypackage'''
+      
 from CapitalsFolder import capitals
 
-capitals.check_capital("Germany")
-capitals.check_capital("Honduras")
-capitals.check_state("Rome")
-capitals.check_state("Tokyo")
+import sys
+
+
+if _name_ == "_main_":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'check':
+            capitalscopia.check_capital("Germany")
+            capitalscopia.check_capital("Honduras")
+            capitalscopia.check_state("Rome")
+            capitalscopia.check_state("Tokyo")
+        else:
+            capitalscopia.check_state(sys.argv[1])
+            capitalscopia.check_capital(sys.argv[1])
+
+
 
