7acc1d783f3f26b1e23cf066f4f7cd0e27e12dab
LaAliss <alice.rigotto@student.h-farm.com>
2019-12-17 16:50:40 -0500

    modified main.py and dbmanager.py

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-#! /usr/bin/env python3\
-
-
-'''from CapitalsFolder import capitals'''
-
 import argparse
 import csv
+import sqlite3
+import hashlib
 
-from CapitalsFolder.csvcode import check_input
+from CSVfolder.csvcode import get_capital_or_state
 
-csv_folder = 'CapitalsFolder/capitals.csv'
+csv_folder = 'CSVfolder/capitals.csv'
 csv_reader = csv.reader(open(csv_folder, 'r'))
+
+conn = None
+cursor = None
+database = 'scripts/ourdatabase.db'
+
+'''
+This function check the username in order to control if it is in the database
+
+'''
+
+
+def check_for_username(username, password):
+
+    global conn
+    global cursor
+
+    conn = sqlite3.connect('scripts/ourdatabase.db')
+    cursor = conn.cursor()
+    salt = cursor.execute("SELECT salt FROM user WHERE username=?", (username,)).fetchall()
+
+    conn.commit()
+
+    hasha = str(salt[0][0]) + password
+
+    for i in range(100000):
+        hasha = hashlib.sha256(hasha.encode('utf-8')).hexdigest()
+
+    rows = cursor.execute("SELECT * FROM user WHERE username=? and hasha=?", (username, hasha))
+
+    conn.commit()
+    results = rows.fetchall()
+    # NOTE: this could be done more efficiently with a JOIN
+    if results:
+        print("User is present, password is valid")
+    else:
+        print("User is not present, or password is invalid")
+
+    cursor.close()
+    conn.close()
+
 
 ''' This function parses as positional argument the State or the Capital.
 Then we define the level of verbosity'''
@@ -17,16 +54,26 @@ Then we define the level of verbosity'''
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('name', help='Please, insert the capital',
-                        'or the state')
-    parser.add_argument('-v', '--verbosity', help='Incrementally',
-                        'increase the verbosity', action='count', default=0)
+    parser.add_argument('name', help='Please, insert the capital or the State')
+    parser.add_argument('-c', help="Check username and password",
+                        required=False)
+    parser.add_argument('-p', help="The username password", required=True)
+    parser.add_argument('-v', '--verbosity', help='Incrementally increase the verbosity', action='count', default=0)
     args = parser.parse_args()
     return args
 
+
+args = parse_arguments()
+
+'''
+This function allow to run the entire program, first checking username and password and then running the function that looks for the country or capital name.
+
+'''
 
 if __name__ == '__main__':
     args = parse_arguments()
     '''capitals.check_capital(args)'''
     '''capitals.check_state(args)'''
-    check_input(args.name)
+    get_name = get_capital_or_state(args.name)
+    check_for_username(args.c, args.p)
+    print(get_name)
--- scripts/dbmanager.py
+++ scripts/dbmanager.py
@@ -3,22 +3,22 @@ import hashlib
 import argparse
 import random
 
-
-''' This function is the one that checks if an already
-    existing database is present in the repository.
-    If not, it will create a table with SQL '''
+'''
+The function named check_or_create will check if an already existing table
+exists. If not a new table called user will be created.
+The table will have three columns called username, hasha and salt
+'''
 
 
 def check_or_create():
 
     global conn
     global cursor
-    conn = sqlite3.connect("ourdatabase.db")
+    conn = sqlite3.connect('ourdatabase.db')
     cursor = conn.cursor()
     try:
         cursor.execute("SELECT * FROM user")
     except sqlite3.OperationalError:
-        '''Create table'''
         cursor.execute('''CREATE TABLE user
                       (username CHAR(256),
                        hasha CHAR(256),
@@ -26,24 +26,24 @@ def check_or_create():
                        PRIMARY KEY (hasha))''')
 
 
-''' Here, we are passing three arguments that
-    the programm will ask the user to insert '''
+'''
+Here we are adding two more optional arguments -a, -p that will
+be used to add a new user to the table.
+'''
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", help="add a usernamename (requires -p)",
-                        required=False)
-    parser.add_argument("-p", help="the username password",
+    parser.add_argument('-a', help="add a usernamename",
+                    required=False)
+    parser.add_argument('-p', help="the username password",
                         required=True)
-    parser.add_argument("-c", help="Check username and password",
-                        required=False)
     return parser.parse_args()
 
 
-''' Once the user has inserted the username
-    and the password, his data will be saved
-    in the database that we have before hand created '''
+'''
+This function will add a new user to our database
+'''
 
 
 def save_new_username(username, password):
@@ -56,56 +56,17 @@ def save_new_username(username, password):
     for i in range(100000):
         hasha = hashlib.sha256(hasha.encode('utf-8')).hexdigest()
 
-    cursor.execute("DELETE FROM user WHERE username=?", (username,))
     cursor.execute("INSERT OR REPLACE INTO user VALUES (?,?,?)",
                    (username, hasha, salt))
     conn.commit()
-    print("The user {} has correctly inserted to ourdatabase".format(username))
+    print('{} has been correctly inserted in ourdatabase'.format(username))
 
 
-''' This function check if the data of the
-    user are contained in ourdatabase. If yes,
-    the user is allowed to have access to the data '''
-
-
-def check_for_username(username, password):
-    global conn
-    global cursor
-
-    conn = sqlite3.connect("ourdatabase.db")
-    cursor = conn.cursor()
-    salt = cursor.execute("SELECT salt FROM user WHERE username=?",
-                          (username,)).fetchall()
-
-    conn.commit()
-
-    hasha = str(salt[0][0]) + password
-
-    for i in range(100000):
-        hasha = hashlib.sha256(hasha.encode('utf-8')).hexdigest()
-
-    rows = cursor.execute("SELECT * FROM user WHERE username=? and hasha=?",
-                          (username, hasha))
-
-    conn.commit()
-    results = rows.fetchall()
-    '''NOTE: this could be done more efficiently with a JOIN'''
-    if results:
-        print("User is present, password is valid")
-    else:
-        print("User is not present, or password is invalid")
-
-    cursor.close()
-    conn.close()
-
-
-args = parse_args()
 check_or_create()
+args = parse_args()
 
 
 if args.a and args.p:
     save_new_username(args.a, args.p)
-elif args.c and args.p:
-    check_for_username(args.c, args.p)
 
 conn.close()
